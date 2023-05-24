6598e8dcc40b1ef9f4e495de949cb1f7ca84ed6d
numdez <76885866+numdez@users.noreply.github.com>
2023-05-24 20:41:16 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,48 +6,56 @@ public class Main {
         int escolhaMenu, logado = 0;
         String userNome, userCPF, userEmail, userSenha, userBairro;
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Insira seu nome: ");
-        userNome = entrada.nextLine();
-        System.out.println("Insira seu cpf: ");
-        userCPF = entrada.nextLine();
-        System.out.println("Insira seu email: ");
-        userEmail = entrada.nextLine();
-        System.out.println("Crie uma senha: ");
-        userSenha = Base64.getEncoder().encodeToString(entrada.nextLine().getBytes());
-        System.out.println("Insira seu bairro: ");
-        userBairro = entrada.nextLine();
+        mae user = new mae("", "", "", "", "");
         
-        mae josue = new mae(userNome, userCPF, userEmail, userSenha, userBairro);
         do{
-            if(josue == null){
-                System.out.println("Não há mais usuários cadastrados no sistema!");
-            }
-            System.out.println("1 - Login \n2 - Apagar conta");
+            System.out.println("1 - Cadastro \n2 - Login \n3 - Apagar conta");
             escolhaMenu = entrada.nextInt();
             entrada.nextLine();
             if(escolhaMenu == 1){
-                System.out.println("Insira o nome de usuário: ");
-                userNome = entrada.nextLine();
-                System.out.println("Insira a senha:");
-                userSenha = Base64.getEncoder().encodeToString(entrada.nextLine().getBytes());
-                logado = josue.loginMae(userNome, userSenha, 0);
-                if(logado == 404){
-                    System.out.println("O usuário não foi encontrado!");
+                if(logado == 0){
+                    System.out.println("Insira seu nome: ");
+                    userNome = entrada.nextLine();
+                    System.out.println("Insira seu cpf: ");
+                    userCPF = entrada.nextLine();
+                    System.out.println("Insira seu email: ");
+                    userEmail = entrada.nextLine();
+                    System.out.println("Crie uma senha: ");
+                    userSenha = Base64.getEncoder().encodeToString(entrada.nextLine().getBytes());
+                    System.out.println("Insira seu bairro: ");
+                    userBairro = entrada.nextLine();
+                    user = new mae(userNome, userCPF, userEmail, userSenha, userBairro);
+
                 }
             }
-            else{
-                assert true;
-            }
             if(escolhaMenu == 2){
+                if(logado == 0){
+                    System.out.println("Insira o nome de usuário: ");
+                    userNome = entrada.nextLine();
+                    System.out.println("Insira a senha:");
+                    userSenha = Base64.getEncoder().encodeToString(entrada.nextLine().getBytes());
+                    logado = user.loginMae(userNome, userSenha, 0);
+                    if(logado == 404){
+                        System.out.println("O usuário não foi encontrado!");
+                    }
+                }
+                else{
+                    System.out.println("O usuário já está logado no sistema!");
+                }
+                userSenha = "";
+            }
+            else if(escolhaMenu == 3){
+                System.out.println("Insira a senha:");
                 userSenha = Base64.getEncoder().encodeToString(entrada.nextLine().getBytes()); 
                 if (logado == 1){
-                    if(josue.deletaUsuario(userSenha) == 1){
-                        josue = null;
+                    if(user.deletaUsuario(userSenha) == 1){
+                        user = null;
                     }
                 }
                 else{
                     System.out.println("O usuário deve estar logado para deletar sua conta!");
                 }
+                userSenha = "";
             }
         }
         while(true);
